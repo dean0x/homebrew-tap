0f17bc7292438ff50b9191ec58b7a6dbd22c8e6a
Dean Sharon <deanshrn@gmail.com>
2026-02-14 10:43:23 +0200

    fix: update mino checksums for re-tagged v1.0.0

--- Formula/mino.rb
+++ Formula/mino.rb
@@ -7,24 +7,24 @@ class Mino < Formula
   on_macos do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-apple-darwin.tar.gz"
-      sha256 "fbadefb1d6a7fc15703d0a32a449f8783e3328f95a93dcb9e259240faa4d30fc"
+      sha256 "b6524501facade5b2d79355e43d0c32fbe6860222efc967d68fc78ffed338cd8"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-apple-darwin.tar.gz"
-      sha256 "91f57ef794b71074b9b3390a7edfaa067ced62938d04ccf726e8ea6c7c87a282"
+      sha256 "f82801f7a07f9c9c61e08b7b88d6dbf55bb3ddcbec1a8c8a334d741a601bbcaf"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "0f9de0bbc4497190e40db367bfc051ce781f57754074fb9f19bffd7e8bbecf92"
+      sha256 "08515276628033e557c3c3e799b780b4cf44381345b5e4f7b457660bc2ef6095"
     end
 
     on_arm do
       url "https://github.com/dean0x/mino/releases/download/v#{version}/mino-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "290cebf6e02a4bad91ba8bf645dc2dd336b2e5fcea35f3f241e36ce69163612d"
+      sha256 "46bd34e6b47dc6011610ed81929371ed73342c8b16753354a6b8fc27b8b6346f"
     end
   end
 
